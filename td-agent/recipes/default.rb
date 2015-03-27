db62f26e86b0376f470d605f4808b6627e4c74ef
takayoshi.harada <harada@takayoshiharada-no-MacBook-Pro.local>
2015-03-27 18:43:46 +0900

    td-agentの設定を追加

---
+++ td-agent/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: td-agent
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+template "/etc/td-agent/td-agent.conf" do
+  source "td-agent.conf"
+  mode   644
+  owner "root"
+  group "root"
+  action :create
+end
+

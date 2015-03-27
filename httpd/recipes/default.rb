6644c371a0232652513f646fd6dcf28ced0f8c65
takayoshi.harada <harada@takayoshiharada-no-MacBook-Pro.local>
2015-03-27 17:37:02 +0900

    create httpd file

---
+++ httpd/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: httpd
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+template "/etc/httpd/conf/httpd.conf" do
+  source "httpd.conf"
+  mode   644
+  owner "root"
+  group "root"
+  action :create
+end

0ac3cc7eafcdbe4972263509aa68ecd6d24c28db
takayoshi.harada <harada@takayoshiharada-no-MacBook-Pro.local>
2015-04-20 08:52:15 +0900

    modified:   td-agent/recipes/opsworks.rb

--- td-agent/recipes/opsworks.rb
+++ td-agent/recipes/opsworks.rb
@@ -7,4 +7,12 @@ template "/etc/td-agent/td-agent.conf" do
   action :create
 end
 
+execute "change permission" do
+  command "chgrp -R td-agent /var/log/{httpd,messages}"
+  command "chmod -R g+rx /var/log/{httpd,messages}"
+  command "chgrp -R td-agent /var/log/httpd/error.log"
+  command "chmod -R g+rx /var/log/httpd/error.log"
+end
+
+
 include_recipe "td-agent::start"

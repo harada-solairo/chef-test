23cfc1fc7caf1c572bff3add0dcfe08e529564ad
takayoshi.harada <harada@takayoshiharada-no-MacBook-Pro.local>
2015-04-22 18:33:41 +0900

    new file:   td-agent/recipes/install_gemfiles.rb

---
+++ td-agent/recipes/install_gemfiles.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: td-agent
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+['fluent-plugin-rewrite-tag-filter','fluent-plugin-parser','fluent-plugin-config-expander','fluent-plugin-record-reformer'].each do | pluginname \
+  gem_package pluginname do
+    gem_binary "/usr/lib64/fluent/ruby/bin/fluent-gem"
+    action :install
+  end
+end
+

07d4cf1a1873cc1d06d265e70be47ff34f0831a6
takayoshi.harada <harada@takayoshiharada-no-MacBook-Pro.local>
2015-04-23 16:28:20 +0900

    modified:   td-agent/recipes/opsworks.rb

--- td-agent/recipes/opsworks.rb
+++ td-agent/recipes/opsworks.rb
@@ -4,6 +4,7 @@ template "/etc/td-agent/td-agent.conf" do
   owner  "td-agent"
   group  "td-agent"
   action :create
+  notifies :restart, "service[td-agent]" 
 end
 
 execute "change permission" do
@@ -12,11 +13,6 @@ execute "change permission" do
   command "sudo chgrp -R td-agent /var/log/httpd"
   command "sudo chmod -R g+rx /var/log/httpd"
   notifies :restart, 'service[td-anget]'
-end
-
-service "td-agent" do
-  service_name "td-agent"
-  restart_command "service httpd restart"
 end
 
 

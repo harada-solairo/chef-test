1b5cfd2dfef1fb93ef4c6b9047d77268edce9dc1
takayoshi.harada <harada@takayoshiharada-no-MacBook-Pro.local>
2015-04-25 12:07:02 +0900

    modified:   httpd/recipes/opsworks.rb

--- httpd/recipes/opsworks.rb
+++ httpd/recipes/opsworks.rb
@@ -23,12 +23,3 @@ template "/etc/httpd/ports.conf" do
   action :create
 end
 
-
-bash 'git config' do
-  #user 'root'
-  code <<-EOL
-    git config --global url."https://".insteadOf git://
-    git config --global http.postBuffer 524288000
-  EOL
-end
-

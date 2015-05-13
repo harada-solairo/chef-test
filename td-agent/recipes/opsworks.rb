f10990a7ce9a42975f15e401777fa039c9325b17
takayoshi.harada <harada@takayoshiharada-no-MacBook-Pro.local>
2015-05-13 11:32:30 +0900

    modified:   td-agent/recipes/opsworks.rb

--- td-agent/recipes/opsworks.rb
+++ td-agent/recipes/opsworks.rb
@@ -10,18 +10,22 @@ end
 
 execute "change permission messange" do
   command "sudo chgrp -R td-agent /var/log/messages"
+  action :run
 end
 
 execute "change permission httpd" do
   command "sudo chmod -R g+rx /var/log/messages"
+  action :run
 end
 
 execute "change owner messange" do
   command "sudo chown -R td-agent /var/log/httpd"
+  action :run
 end
 
 execute "change owner httpd" do
   command "sudo chmod -R g+rx /var/log/httpd"
+  action :run
 end
 
 service "td-agent" do
